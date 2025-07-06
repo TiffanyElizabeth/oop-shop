8c687f5181b92f672537eb56f59fc87e33d4d8e8
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-07-06 18:18:56 +0200

    first commit

---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,12 @@
+package org.lessons.java.shop;
+
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+        Prodotto test = new Prodotto("test", "test description", new BigDecimal("39.99"), new BigDecimal("0.22"));
+
+        System.out.println(test.generateProductCode());
+        System.out.println(test.generateCodeName());
+    }
+}
---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,55 @@
+package org.lessons.java.shop;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Random;
+
+public class Prodotto {
+
+    public String productCode;
+    public String productName;
+    public String productDescription;
+    public BigDecimal productBasePrice;
+    public BigDecimal iva;
+    public BigDecimal productFinalPrice;
+
+    // method to generate random product code
+    public String generateProductCode() {
+        Random random = new Random(); // generate random numbers
+        int productNumber = random.nextInt(999999) + 1; // to give us numbers between 1 and 999999
+        return String.format("%06d", productNumber); // to format with leading zeros
+    }
+
+    // method to generate final price - can't use normal math operators with
+    // BigDecimal / setScale gives 2 decimal points
+    public BigDecimal generateFinalPrice() {
+        return productBasePrice.add(productBasePrice.multiply(iva)).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    // method to give code + name
+    public String generateCodeName() {
+        return productCode + ": " + productName;
+    }
+
+    // constructor 1 (randomly generated product code)
+    Prodotto(String productName, String productDescription, BigDecimal productBasePrice, BigDecimal iva) {
+        this.productCode = generateProductCode();
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.productBasePrice = productBasePrice;
+        this.iva = iva;
+        this.productFinalPrice = generateFinalPrice();
+    }
+
+    // constructor 2 (predefined product code)
+    Prodotto(String productCode, String productName, String productDescription, BigDecimal productBasePrice,
+            BigDecimal iva) {
+        this.productCode = productCode;
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.productBasePrice = productBasePrice;
+        this.iva = iva;
+        this.productFinalPrice = generateFinalPrice();
+    }
+
+}
